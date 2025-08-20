43f27f01b6f23337f4c2086d1e5e1f4796575aa3
parsa <parsazoli3448@gmail.com>
2025-08-20 12:46:46 +0430

    add Click Handler for button of box

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,9 @@
 const boxEl = document.querySelector('.product');
-const btnOfBoxEl = document.querySelector('.btn-product');
-
-const clickOfBoxHandler = event => {
-    
-    event.preventDefault();
-    const targetOfEvent = btnOfBoxEl.target;
-
-    if(event.target.closest('.product')){
-        console.log(event);
-    }
+const btnOfBoxEl = document.querySelectorAll('.btn-product'); 
+for (let index = 0; index < btnOfBoxEl.length; index++) {
+    btnOfBoxEl[index].addEventListener('click', clickOfBoxHandler = event => {
+        event.preventDefault();
+        const targetOfEvent = event.target.closest('.product');
+        console.log(targetOfEvent);
+    });
 }
-
-btnOfBoxEl.addEventListener('click' , clickOfBoxHandler);
