950fb3696da8d96f46ee4d73ea55ec2d357e9778
ImanAsefi <ImanAsefi9022@gmail.com>
2025-08-21 12:47:18 +0330

    test for total-price

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,22 @@
 const boxEl = document.querySelector('.product');
 const btnOfBoxEl = document.querySelectorAll('.btn-product'); 
+const itemsOfCartEl = document.querySelector('.items');
+const priceOfCartEl = document.querySelectorAll('.price');
+const priceIntOfCartEl = document.querySelectorAll('.price-Int');
+
 for (let index = 0; index < btnOfBoxEl.length; index++) {
     btnOfBoxEl[index].addEventListener('click', clickOfBoxHandler = event => {
         event.preventDefault();
         const targetOfEvent = event.target.closest('.product');
-        console.log(targetOfEvent);
+        
+        const targetOfEventEl = targetOfEvent;
+        itemsOfCartEl.insertAdjacentElement('beforeend',targetOfEventEl);
+        
+        for(let i = 0;i < btnOfBoxEl.length;i++){
+            btnOfBoxEl[i].addEventListener('click' , totalPriceInt = event => {
+                const priceIntOfCart = priceIntOfCartEl[i].target.innerHTML;
+                console.log(priceIntOfCart);
+            });
+        };
     });
 }
