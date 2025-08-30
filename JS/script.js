2b4eff0ef5254b1d23d6a7a46295c422908add16
ImanAsefi <ImanAsefi9022@gmail.com>
2025-08-30 13:06:04 +0330

    add owl

--- JS/script.js
+++ JS/script.js
@@ -17,3 +17,31 @@ for (let index = 0; index < btnOfBoxEl.length; index++) {
 }
 
 
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    loop:true,
+    nav:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },            
+        960:{
+            items:5
+        },
+        1200:{
+            items:6
+        }
+    }
+});
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
