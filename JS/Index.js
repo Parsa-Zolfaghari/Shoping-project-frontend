e7e0484efe5aaa300179f9f02b4dd6ff653e81ff
Iman <ImanAsefi9022@gmail.com>
2026-09-05 12:23:24 +0330

    Save local changes

--- JS/Index.js
+++ JS/Index.js
@@ -2,22 +2,3 @@ const headerSearchBar = document.querySelector(".header-nav_search");
 const headerNavBtnBars = document.querySelector(".header-nav_btn_bars");
 const headerBtnBars = document.querySelector(".header-btn_bars");
 
-
-const setDisplaySearch = headerSearchBar.style.display = "none";
-const setDisplayBtnBars = headerBtnBars.style.display = "none";
-function searchBtnClick () {
-    if (headerSearchBar.style.display == "none") {
-        headerSearchBar.style.display = "flex";
-    } else{
-        headerSearchBar.style.display = "none";
-    }
-}
-
-
-function headerMenuResposive () {
-    if (headerBtnBars.style.display == "none") {
-        headerBtnBars.style.display = "block";
-    } else {
-        headerBtnBars.style.display = "none";
-    }
-}
